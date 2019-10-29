49a2890daaa0348b6a226e2e12cba8041bc8d0bd
Vasily Moskaev <moskaevvv@mail.ru>
2019-10-29 11:28:17 +0300

    Обновил получение xml

--- getSchedule.php
+++ getSchedule.php
@@ -2,7 +2,7 @@
 $remote_file_contents = file_get_contents('http://www.aviamordovia.ru/schedule/Schedule.xml');
 //Get the contents
 
-$local_file_path = 'Schedule.xml';
+$local_file_path = 'skx/public_html/Schedule.xml';
 
 file_put_contents($local_file_path, $remote_file_contents);
 //save the contents of the remote file
